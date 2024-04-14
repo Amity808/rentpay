e006e44ac33bd9d670871f1a44ec811cf395a8c6
amity808 <bolarinwamuhdsodiq0@gmail.com>
2024-04-14 23:42:39 +0100

    backend logic

---
+++ backend/src/utils/redis_connect.ts
@@ -0,0 +1,33 @@
+const redis = require('redis')
+
+const client = redis.createClient({
+    port: 17682,
+    host: process.env.REDIS_HOST,
+    password: process.env.REDIS_PASSWORD, 
+})
+
+client.on('connect', () => {
+    console.log('Client connect to rdis...');
+    
+})
+
+client.on('ready', () => {
+    console.log('Connect to redis, ready to use');
+})
+
+client.on('error', (err) => {
+    console.log(err.message)
+})
+
+client.on('end', () => {
+    console.log('Client disconnected from redis');
+})
+
+
+client.on('SIGINT', () => {{
+    client.quit()
+
+}})
+
+
+module.exports = client
