863a8571412b5b3c3512bfe39debf0798690dd7f
AJTECH <aladejamiudamilola@gmail.com>
2024-04-15 22:12:29 +0100

    landiing page updated

--- next-app/components/Header.tsx
+++ next-app/components/Header.tsx
@@ -43,23 +43,10 @@ export default function Header() {
                                 </Disclosure.Button>
                             </div>
                             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                                <div className="flex flex-shrink-0 items-center">
-                                    <Image
-                                        className="block h-8 w-auto sm:block lg:block"
-                                        src="/logo.svg"
-                                        width="24"
-                                        height="24"
-                                        alt="Celo Logo"
-                                    />
+                                <div className="flex flex-shrink-0 text-5xl items-center">
+                                   RENTPAY
                                 </div>
-                                <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-                                    <a
-                                        href="#"
-                                        className="inline-flex items-center border-b-2 border-black px-1 pt-1 text-sm font-medium text-gray-900"
-                                    >
-                                        Home
-                                    </a>
-                                </div>
+                                
                             </div>
                             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                                 {!hideConnectBtn && (
