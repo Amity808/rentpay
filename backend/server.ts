1cffcb4b5fda102a7b2e2262b36a7cb4054afc12
amity808 <bolarinwamuhdsodiq0@gmail.com>
2024-04-15 07:29:05 +0100

    implementing  Backend

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,31 @@
 
+const express = require('express')
+const http = require('http')
+const cors = require('cors')
+const config = require('dotenv')
+config.config()
+const logger = require('logger')
+
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
+app.use(logger('dev'))
+
+const port = 3000
+app.set("port", port)
+app.set('view engine', 'ejs')
+// app.set('views')
+
+const server = http.createServer(app)
+
+app.get("/", (req, res) => {
+    res.send("Hello world")
+})
+
+
+server.listen(port, '0.0.0.0')
+server.on('listening', () => {
+    console.log('Server is running on port', port)
+})
