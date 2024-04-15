3927157a881cab4c482844710087c91a0942b345
amity808 <bolarinwamuhdsodiq0@gmail.com>
2024-04-15 07:29:05 +0100

    implementing  Backend

--- backend/src/controllers/authControllers.ts
+++ backend/src/controllers/authControllers.ts
@@ -2,11 +2,11 @@ const createError = require('http-errors')
 const User = require('../models/User')
 const JWT = require('jsonwebtoken')
 const mongoose = require('mongoose')
-const client = require('../utils/redis_connect')
+const clientRed = require('../utils/redis_connect')
 const messages = require("../helpers/messages")
 const utils = require("../utils/utilsfn")
 const dbConnect = require("../utils/dbconnect")
-const validations = require("../utils/validators")
+const validations_ = require("../utils/validators")
 const bcrypt = require("bcrypt")
 
 
@@ -17,7 +17,7 @@ const authRoutes = {
         const session = await mongoose.startSession()
 
         try {
-            const result = await validations.authSchema.validateAsync(req.body)
+            const result = await validations_.authSchema.validateAsync(req.body)
             const user = await User.findOne({ email: result.email }).session(session)
             if(user) {
                 throw createError.Conflict(utils.joinStringWithSpace([ result.email, message.auth.alreadyExistPartText]))
@@ -34,6 +34,21 @@ const authRoutes = {
                 error.status = 422;
             }
             next()
+        }
+    },
+
+    login: async (req, res, next) => {
+        try {
+            const result = await validations_.authSchema.validateAsync(req.body)
+            const user = await User.findOne({ email: result.email })
+            if(!user) throw createError.MotFound(messages.auth.userNotRegistered)
+            console.log(result);
+            const isMatch = await User.isValidPassword(result.password)
+            if(!isMatch)
+                throw createError.Unauthorized(messages.auth.emailNotVerified)
+            const accessToken = await jwtHelper
+        } catch (error) {
+            
         }
     }
 }
