c120a9fbf3b6ea07b1adfcae64c58954b1528a3b
Micholn <mdiagbare@gmail.com>
2024-04-17 07:22:50 +0100

    moved all the folders to sync in the absence of the backend

--- next-app/contracts/RentPay.sol
+++ next-app/contracts/RentPay.sol
@@ -1,34 +1,70 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+contract RentPay {
+    address public owner;
+    mapping(address => uint256) public balances;
+    mapping(address => uint256) public lockedFunds;
+    mapping(address => uint256) public autosavePercentage;
+    mapping(address => mapping(address => uint256)) public contributions;
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+    event RentPaid(address indexed tenant, uint256 amount);
+    event UtilitiesPaid(address indexed tenant, uint256 amount);
+    event FundLocked(address indexed tenant, uint256 amount);
+    event AutosaveConfigured(address indexed tenant, uint256 percentage);
+    event ContributionReceived(address indexed contributor, address indexed tenant, uint256 amount);
 
-    event Withdrawal(uint amount, uint when);
-
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+    constructor() {
+        owner = msg.sender;
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+    function payRent() external payable {
+        require(balances[msg.sender] >= msg.value, "Insufficient funds");
+        balances[msg.sender] -= msg.value;
+        emit RentPaid(msg.sender, msg.value);
+    }
 
-        emit Withdrawal(address(this).balance, block.timestamp);
+    function payUtilities() external payable {
+        require(balances[msg.sender] >= msg.value, "Insufficient funds");
+        balances[msg.sender] -= msg.value;
+        emit UtilitiesPaid(msg.sender, msg.value);
+    }
 
-        owner.transfer(address(this).balance);
+    function lockFunds(uint256 amount) external {
+        require(balances[msg.sender] >= amount, "Insufficient funds");
+        balances[msg.sender] -= amount;
+        lockedFunds[msg.sender] += amount;
+        emit FundLocked(msg.sender, amount);
+    }
+
+    function configureAutosave(uint256 percentage) external {
+        require(percentage <= 100, "Percentage must be less than or equal to 100");
+        autosavePercentage[msg.sender] = percentage;
+        emit AutosaveConfigured(msg.sender, percentage);
+    }
+
+    function contribute(address tenant) external payable {
+        contributions[tenant][msg.sender] += msg.value;
+        emit ContributionReceived(msg.sender, tenant, msg.value);
+    }
+
+    function withdraw() external {
+        uint256 amount = balances[msg.sender] + lockedFunds[msg.sender];
+        require(amount > 0, "No funds to withdraw");
+        balances[msg.sender] = 0;
+        lockedFunds[msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
+    }
+
+    function withdrawContributions() external {
+        uint256 amount = contributions[msg.sender][msg.sender];
+        require(amount > 0, "No contributions to withdraw");
+        contributions[msg.sender][msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
     }
 }
