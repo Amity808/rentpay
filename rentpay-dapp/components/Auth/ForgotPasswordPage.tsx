3507f91a1237f3696e3015a3530f7df4781a042e
Micholn <mdiagbare@gmail.com>
2024-04-20 00:41:35 +0100

    factored the ForgotPasswordPage.tsx

--- rentpay-dapp/components/Auth/ForgotPasswordPage.tsx
+++ rentpay-dapp/components/Auth/ForgotPasswordPage.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 
 const ForgotPasswordPage: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-  
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
   return (
     <div>
       <h1>Forgot Password Page</h1>
