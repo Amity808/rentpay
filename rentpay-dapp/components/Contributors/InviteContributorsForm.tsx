a4109246097003df5a145ef01ea22d5d08cfc262
Micholn <mdiagbare@gmail.com>
2024-04-19 19:59:57 +0100

    updated the Payments (PaymentForm.tsx, PaymentHistory.tsx) Savings (SavingsAccount.ts) Autosave, (AutosaveSettings.tsx) Contributors (InviteContributorsForm.tsx, ContributorsList.tsx)

---
+++ rentpay-dapp/components/Contributors/InviteContributorsForm.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const InviteContributorsForm: React.FC = () => {
+  //logic for invite contributors form
+  return (
+    <div>
+      <h1>Invite Contributors Form</h1>
+      {/*components and logic for inviting contributors */}
+    </div>
+  );
+}
+
+export default InviteContributorsForm;
---
+++ rentpay-dapp/components/Dashboard/Payments.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Payments: React.FC = () => {
+    return (
+        <div>
+            <h1>Payments Page</h1>
+            {/** Payments content */}
+        </div>
+    );
+}
+
+export default Payments;
