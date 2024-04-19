93437aec85e8769e4d9c05426210577736f411dc
Micholn <mdiagbare@gmail.com>
2024-04-19 19:40:28 +0100

    updated the dashboard and overview files

---
+++ rentpay-dapp/components/Auth/SignupPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SignupPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Signup Page</h1>
+      {/* signup form and logic */}
+    </div>
+  );
+}
+
+export default SignupPage;
