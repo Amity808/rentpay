f204963c73934db355f10dcd083eda85049d98cf
Micholn <mdiagbare@gmail.com>
2024-04-20 00:36:44 +0100

    wrote the Logic for the ForgotPasswordPage.tsx

--- rentpay-dapp/components/Auth/ForgotPasswordPage.tsx
+++ rentpay-dapp/components/Auth/ForgotPasswordPage.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ForgotPasswordPage: React.FC = () => {
+
+  
   return (
     <div>
       <h1>Forgot Password Page</h1>
