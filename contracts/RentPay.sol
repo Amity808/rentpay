83ea58d50b5ef00e6fabd4b2f87f190a1dba95cf
Micholn <mdiagbare@gmail.com>
2024-04-20 23:34:25 +0100

    edited smart contract

--- contracts/RentPay.sol
+++ contracts/RentPay.sol
@@ -73,7 +73,6 @@ contract RentPay {
         uint256 totalAmount;
         if (keccak256(abi.encodePacked(asset)) == keccak256(abi.encodePacked("ETH"))) {
             totalAmount = balances[msg.sender] + lockedFunds[msg.sender];
-            require(totalAmount >= amount, "Insufficient balance");
             
             payable(msg.sender).transfer(amount);
         } else {
