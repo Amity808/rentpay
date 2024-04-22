1395f22d8bf80bfe952f2248ad1a100a5b92834e
Micholn <mdiagbare@gmail.com>
2024-04-22 21:04:44 +0100

    added a new homepage

--- rentpay-dapp/components/Common/Header.tsx
+++ rentpay-dapp/components/Common/Header.tsx
@@ -23,26 +23,8 @@ export default function Header() {
                 <>
                     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                         <div className="relative flex h-16 justify-between">
-                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                                {/* Mobile menu button */}
-                                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-black focus:outline-none focus:ring-1 focus:ring-inset focus:rounded-none focus:ring-black">
-                                    <span className="sr-only">
-                                        Open main menu
-                                    </span>
-                                    {open ? (
-                                        <XMarkIcon
-                                            className="block h-6 w-6"
-                                            aria-hidden="true"
-                                        />
-                                    ) : (
-                                        <Bars3Icon
-                                            className="block h-6 w-6"
-                                            aria-hidden="true"
-                                        />
-                                    )}
-                                </Disclosure.Button>
-                            </div>
-                            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                            
+                            <div className="flex  items-center justify-center ">
                                 <div className="flex flex-shrink-0 text-5xl items-center">
                                    RENTPAY
                                 </div>
@@ -61,18 +43,7 @@ export default function Header() {
                         </div>
                     </div>
 
-                    <Disclosure.Panel className="sm:hidden">
-                        <div className="space-y-1 pt-2 pb-4">
-                            <Disclosure.Button
-                                as="a"
-                                href="#"
-                                className="block border-l-4 border-black py-2 pl-3 pr-4 text-base font-medium text-black"
-                            >
-                                Home
-                            </Disclosure.Button>
-                            {/* Add here your custom menu elements */}
-                        </div>
-                    </Disclosure.Panel>
+                   
                 </>
             )}
         </Disclosure>
